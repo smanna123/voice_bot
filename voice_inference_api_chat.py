c193b4ffb4caa46ccb2824dcb5a42192721abf27
smanna123 <sayantanmanna840@gmail.com>
2024-08-23 18:21:34 +0530

    cors issue solved

--- voice_inference_api_chat.py
+++ voice_inference_api_chat.py
@@ -22,10 +22,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],  # Allows all origins
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
 )
 
 # Setup logging
